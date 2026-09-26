f52c861f41a64a13092edd8b3db9845b0dbb67d8
D31ux3 <86838255+D31ux3@users.noreply.github.com>
2026-09-26 00:22:06 -0300

    dark mode

--- Configs/.config/hypr/conf/autostart.lua
+++ Configs/.config/hypr/conf/autostart.lua
@@ -1,6 +1,9 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 hl.on("hyprland.start", function()
+    -- Dark mode for GTK/libadwaita apps and apps that follow the portal (Firefox, Brave, Electron)
+    hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
+    hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'")
     hl.exec_cmd("waybar")
     hl.exec_cmd("swaync")
     hl.exec_cmd("~/.config/hypr/scripts/wallpaper.sh")
--- Configs/.config/hypr/conf/env.lua
+++ Configs/.config/hypr/conf/env.lua
@@ -2,3 +2,6 @@
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+-- Qt apps use qt6ct for theming (dark palette, see ~/.config/qt6ct/qt6ct.conf)
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
